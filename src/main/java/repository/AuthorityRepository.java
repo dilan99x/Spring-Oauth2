7ccbe980240525df437f5de418bc649e3fb34b33
UANUSDI <dilan.anushka@pearson.com>
2017-03-18 14:20:29 +0530

    initial commit

---
+++ src/main/java/repository/AuthorityRepository.java
@@ -0,0 +1,7 @@
+package repository;
+
+import domain.Authority;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
